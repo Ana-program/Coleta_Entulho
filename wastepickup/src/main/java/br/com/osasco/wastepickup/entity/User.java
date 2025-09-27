5249a091951baaef855e5a0d47e4328deaaba87e
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-27 01:34:47 -0300

    Adiciona atributos

--- wastepickup/src/main/java/br/com/osasco/wastepickup/entity/User.java
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/entity/User.java
@@ -1,20 +1,16 @@
 package br.com.osasco.wastepickup.entity;
 
-import br.com.osasco.wastepickup.dto.AuthenticationDTO;
-import br.com.osasco.wastepickup.dto.RegisterDTO;
 import br.com.osasco.wastepickup.role.Role;
 import jakarta.persistence.*;
-import jakarta.validation.Valid;
-import lombok.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
@@ -29,15 +25,41 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String password;
     private String login;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
     private String email;
     private String name;
     private String address;
     private String phone;
     private Boolean active;
+
+    @Column(length = 11)
+    private String cpf;
+
+    @Column(length = 20)
+    private String rg;
+
+    @Column(length = 10)
+    private String gender;
+
+    @Column(name = "zip_code", length = 10)
+    private String zipCode;
+
+    private String street;
+    private String number;
+    private String complement;
+    private String neighborhood;
+    private String city;
+    private String state;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(length = 15)
+    private String mobile;
 
     public User (String login, String password, Role role){
         this.login = login;
@@ -47,9 +69,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (this.role == Role.USER)
-            return List.of(new SimpleGrantedAuthority("ROLE_USER"), new SimpleGrantedAuthority("ROLE_ADMIN"));
-        else return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        if (this.role == Role.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
