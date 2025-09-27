f246bfd7d4cf9bd4b10ccde73c377bbb541bfbfa
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-27 01:10:43 -0300

    Atualiza AuthenticationController

--- wastepickup/src/main/java/br/com/osasco/wastepickup/controller/AuthenticationController.java
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/controller/AuthenticationController.java
@@ -1,24 +1,30 @@
 package br.com.osasco.wastepickup.controller;
 
 import br.com.osasco.wastepickup.dto.AuthenticationDTO;
+import br.com.osasco.wastepickup.dto.LoginResponseDTO;
 import br.com.osasco.wastepickup.dto.RegisterDTO;
 import br.com.osasco.wastepickup.entity.Admin;
 import br.com.osasco.wastepickup.entity.User;
+import br.com.osasco.wastepickup.exception.BusinessException;
+import br.com.osasco.wastepickup.infra.security.TokenService;
+import br.com.osasco.wastepickup.mapper.AdminMapper;
+import br.com.osasco.wastepickup.mapper.UserMapper;
 import br.com.osasco.wastepickup.repository.AdminRepository;
 import br.com.osasco.wastepickup.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/auth")
 public class AuthenticationController {
 
     @Autowired
@@ -30,35 +36,62 @@ public class AuthenticationController {
     @Autowired
     private AdminRepository adminRepository;
 
-    @PostMapping("/loginUser")
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    AdminMapper adminMapper;
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
 
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+    
+
+    @PostMapping("/register-user")
+    public ResponseEntity<?> registerUser(@RequestBody @Valid RegisterDTO data) {
+        if (repository.findByLogin(data.login()) != null
+                || adminRepository.findByLogin(data.login()) != null) {
+            throw new BusinessException("Login already exists");
+        }
+
+        String encryptedPassword = passwordEncoder.encode(data.password());
+
+        User newUser = new User();
+        userMapper.builderUser(newUser, data, encryptedPassword);
+        repository.save(newUser);
+
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/registerUser")
-    public ResponseEntity registerUser(@RequestBody @Valid RegisterDTO data) {
-        if (this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+    @PostMapping("/register-admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> registerAdmin(@RequestBody @Valid RegisterDTO data) {
+        if (repository.findByLogin(data.login()) != null
+                || adminRepository.findByLogin(data.login()) != null) {
+            throw new BusinessException("Login already exists");
+        }
 
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        User newUser = new User(data.login(), encryptedPassword, data.role());
-        this.repository.save(newUser);
+        String encryptedPassword = passwordEncoder.encode(data.password());
 
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/registerAdmin")
-    public ResponseEntity registerAdmin(@RequestBody @Valid RegisterDTO data) {
-        if (this.adminRepository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
-
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        Admin newAdmin = new Admin(data.login(), encryptedPassword, data.role());
-        this.adminRepository.save(newAdmin);
+        Admin newAdmin = new Admin();
+        adminMapper.builderAdmin(newAdmin, data, encryptedPassword);
+        adminRepository.save(newAdmin);
 
         return ResponseEntity.ok().build();
     }
+
 
 
 }
