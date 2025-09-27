45ba43971460634f5a580bf5b975e8e9a16205d7
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-27 15:11:48 -0300

    Adiciona Atributos a classe PickupRequestDTO

--- wastepickup/src/main/java/br/com/osasco/wastepickup/dto/PickupRequestDTO.java
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/dto/PickupRequestDTO.java
@@ -1,12 +1,25 @@
 package br.com.osasco.wastepickup.dto;
 
+import br.com.osasco.wastepickup.model.RequestStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class PickupRequestDTO {
 
     private String requesterName;
-    private String address;
     private String debrisType;
     private double estimatedQuantity;
     private LocalDate requestDate;
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private RequestStatus status = RequestStatus.PENDING;
 }
