c96aa988b2170735eae4069a69612e622be828d5
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-27 01:04:18 -0300

    Adiciona atributos

--- wastepickup/src/main/java/br/com/osasco/wastepickup/entity/Admin.java
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/entity/Admin.java
@@ -1,17 +1,12 @@
 package br.com.osasco.wastepickup.entity;
 
-import br.com.osasco.wastepickup.role.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -19,55 +14,30 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table( name = "admin" )
-public class Admin implements UserDetails {
+public class Admin {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email;
+    private String login;
     private String password;
     private String name;
-    private String address;
+    private String cpf;
+    private String rg;
+    private LocalDate birthDate;
+    private String gender;
+    private String zipCode;
+    private String street;
+    private String number;
+    private String complement;
+    private String neighborhood;
+    private String city;
+    private String state;
     private String phone;
-    private String login;
-    private Role role;
+    private String mobile;
+    private String email;
 
-    public Admin (String login, String password, Role role){
-        this.login = login;
-        this.password = password;
-        this.role = role;
-    }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (this.role == Role.ADMIN)
-            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
-        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
 
-    @Override
-    public String getUsername() {
-        return login;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
