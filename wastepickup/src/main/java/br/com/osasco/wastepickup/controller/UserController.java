b2092e321dc1fa32da5045ead588f5192638af7d
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-27 01:39:23 -0300

    Atualiza classe UserController

--- wastepickup/src/main/java/br/com/osasco/wastepickup/controller/UserController.java
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/controller/UserController.java
@@ -1,11 +1,12 @@
 package br.com.osasco.wastepickup.controller;
 
 import br.com.osasco.wastepickup.dto.UserDTO;
-import br.com.osasco.wastepickup.entity.User;
 import br.com.osasco.wastepickup.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,20 +15,26 @@ public class UserController {
     @Autowired
     UserService service;
 
-    @GetMapping
-    public UserDTO getUser(@PathVariable Long id){
-       return service.getUser(id);
-    }
-
     @PutMapping("/{id}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO dto) {
+    public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @RequestBody UserDTO dto) {
         UserDTO updatedUser = service.updateUser(id, dto);
         return ResponseEntity.ok(updatedUser);
     }
 
-    @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody User user){
-        service.createUser(user);
-        return ResponseEntity.noContent().build();
+    @GetMapping("/{id}")
+    public UserDTO getUser(@PathVariable Long id){
+       return service.getUser(id);
     }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAllUsers() {
+        List<UserDTO> users = service.findAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        service.deleteUser(id);
+    }
+
 }
