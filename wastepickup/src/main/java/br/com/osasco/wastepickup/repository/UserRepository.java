ea9b51fd33270e7eaa3947db7740f24e208657ed
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-27 01:35:55 -0300

    Remove importe não utilizado

--- wastepickup/src/main/java/br/com/osasco/wastepickup/repository/UserRepository.java
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package br.com.osasco.wastepickup.repository;
 
-import br.com.osasco.wastepickup.dto.UserDTO;
 import br.com.osasco.wastepickup.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
