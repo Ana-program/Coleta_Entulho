5eb83330b5642c64ff4b42607cc207e06c38eeb6
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-27 15:09:51 -0300

    Cria a classe PickupRequestMapper com os métodos toEntity e toDto

---
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/mapper/PickupRequestMapper.java
@@ -0,0 +1,31 @@
+package br.com.osasco.wastepickup.mapper;
+
+import br.com.osasco.wastepickup.dto.PickupRequestDTO;
+import br.com.osasco.wastepickup.entity.PickupRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PickupRequestMapper {
+
+    public PickupRequest toEntity(PickupRequestDTO dto) {
+
+        PickupRequest entity = new PickupRequest();
+        entity.setAddress(dto.getAddress());
+        entity.setDebrisType(dto.getDebrisType());
+        entity.setEstimatedQuantity(dto.getEstimatedQuantity());
+        entity.setRequestDate(dto.getRequestDate());
+        entity.setStatus(dto.getStatus());
+        return entity;
+    }
+
+    public PickupRequestDTO toDto(PickupRequest entity) {
+        PickupRequestDTO dto = new PickupRequestDTO();
+        dto.setAddress(entity.getAddress());
+        dto.setDebrisType(entity.getDebrisType());
+        dto.setEstimatedQuantity(entity.getEstimatedQuantity());
+        dto.setRequestDate(entity.getRequestDate());
+        dto.setStatus(entity.getStatus());
+        return dto;
+    }
+
+}
