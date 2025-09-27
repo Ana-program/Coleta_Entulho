73de2d54be19401ac320324742cf00c19ff5e804
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-27 01:38:52 -0300

    Atualiza classe SecurityConfigurations

--- wastepickup/src/main/java/br/com/osasco/wastepickup/infra/security/SecurityConfigurations.java
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/infra/security/SecurityConfigurations.java
@@ -1,20 +1,26 @@
 package br.com.osasco.wastepickup.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -22,11 +28,15 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.PUT, "/api/admin/requests/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/admin/requests/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "api/requests/updateStaus").hasRole("ADMIN")
+                        .requestMatchers("/auth/register-user").permitAll()
+                        .requestMatchers("/auth/register-admin").hasRole("ADMIN")
+                        .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/requests/updateStaus").hasRole("ADMIN")
+                        .requestMatchers("/api/admin/requests/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
