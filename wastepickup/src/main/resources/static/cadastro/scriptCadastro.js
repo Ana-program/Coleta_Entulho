58e975b1f22cabc5e3851b21b9dce2c88de1c4b4
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-10-15 15:46:59 -0300

    Cria tela de cadastro de usuario

---
+++ wastepickup/src/main/resources/static/cadastro/scriptCadastro.js
@@ -0,0 +1,51 @@
+const baseUrl = "/auth";
+
+const getValue = id => document.getElementById(id)?.value;
+
+const cadastro = () => ({
+    name: getValue("name"),
+    cpf: getValue("cpf"),
+    rg: getValue("rg"),
+    birthDate: getValue("birthDate"),
+    gender: getValue("gender"),
+    role: getValue("role"),
+    zipCode: getValue("zipCode"),
+    street: getValue("street"),
+    number: getValue("number"),
+    complement: getValue("complement"),
+    neighborhood: getValue("neighborhood"),
+    city: getValue("city"),
+    state: getValue("state"),
+    phone: getValue("phone"),
+    mobile: getValue("mobile"),
+    email: getValue("email"),
+    login: getValue("login"),
+    password: getValue("password")
+});
+
+const handleResponse = (res, successMsg, errorMsg) => {
+    if (res.ok) alert(successMsg);
+    else alert(errorMsg);
+};
+
+document.querySelector("#cadastrar-usuario")?.addEventListener("click", () => {
+console.log(cadastro())
+
+    fetch(`${baseUrl}/register-user`, {
+      method: "POST",
+        headers: { "Content-Type": "application/json" },
+       body: JSON.stringify(cadastro())
+    })
+
+document.querySelector("#cadastrar-usuario")?.addEventListener("click", () => {
+console.log(cadastro())
+
+    fetch(`${baseUrl}/register-admin`, {
+      method: "POST",
+        headers: { "Content-Type": "application/json" },
+       body: JSON.stringify(cadastro())
+    })
+});
+
+
+
