bc98082f020db22ed1c46716c8fe7eefa82e6b92
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-10-28 13:15:21 -0300

    Adiciona método findByUserId

--- wastepickup/src/main/java/br/com/osasco/wastepickup/repository/PickupRequestRepository.java
+++ wastepickup/src/main/java/br/com/osasco/wastepickup/repository/PickupRequestRepository.java
@@ -3,7 +3,11 @@ package br.com.osasco.wastepickup.repository;
 import br.com.osasco.wastepickup.entity.PickupRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PickupRequestRepository extends JpaRepository<PickupRequest, Long> {
 
     boolean existsByAddress(String address);
+
+    List<PickupRequest> findByUserId(Long userId);
 }
